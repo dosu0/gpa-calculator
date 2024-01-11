7e107418301d95656fd95305ac81a1d258462778
plat-phoenix <samuel.a.dosunmu@gmail.com>
2024-01-11 14:55:51 +0000

    Move footer to bottom of page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,38 @@
+<div class="page-container">  
+<div class="wrap-content">
+    
 <nav>
     <a href="/">home</a>
     <a href="/about">about</a>
+    <button>import grades from infinite campus</button>
 </nav>
 
 <h1>GPA Calculator</h1>
 
+
 <slot />
 
 <footer>By Samuel and Robbie 2023-2024</footer>
+        
+</div>
+</div>
 
+
+
+<style>
+    #page-container {
+        position: relative;
+        min-height: 100vh;
+    }
+
+    #wrap-content {
+        padding-bottom: 3.5rem;
+    }
+    
+    footer {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: 2.5rem;
+    }
+</style>
