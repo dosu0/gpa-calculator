ad02c2af274d195dbccd9df518a552614ca09690
plat-phoenix <samuel.a.dosunmu@gmail.com>
2024-01-21 22:15:24 +0000

    Add help page info

--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -1,3 +1,9 @@
-<p>How to use</p>
-<p> Add a class </p>
-<p> - click on text box </p>
+<p></p>
+<p> How to Add a class </p>
+<p> - click on text box that says "enter a subject..." </p>
+<p> - Type the Subject of your class </p>
+<p> How to Change grade </p>
+<p> - Look for the subject of the grade of which you plan to change </p>
+<p> - On the right of that subject should be a white text box with a grade, simply click on it and input a new grade </p>
+<p> - Or click on the up and down arrow </p>
+<p></p>
