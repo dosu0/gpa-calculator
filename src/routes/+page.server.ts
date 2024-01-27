7cf4282bf3467241133e19c9eea6fe4ae6bba0ba
phoenix <samuel.a.dosunmu@gmail.com>
2024-01-27 00:34:48 -0500

    Add uuid on the backend

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,7 @@
 import type District from "$lib/District";
 import User from "$lib/InfiniteCampus";
-import { json } from "@sveltejs/kit";
-
-const user = new User();
-
-user.on("ready", async (user: User) => {
-    console.log(`successfully logged in user ${user}`);
-});
+import type { Term, Course } from "$lib/InfiniteCampus";
+import { v4 as uuid } from "uuid";
 
 async function fetchDistricts() {
     const url = `https://mobile.infinitecampus.com/api/district/searchDistrict?query=county&state=GA`;
@@ -19,23 +14,32 @@ async function fetchDistricts() {
 
 type Load = {
     districts: District[];
+    courses: Term[];
 };
 
-export async function load(): Promise<Load> {
+const db: Map<string, Term[]> = new Map();
+
+export async function load({ cookies }): Promise<Load> {
     // TODO: log user in
     // await user.login("Fulton County", "GA", "userid", "password");
 
     return {
         districts: await fetchDistricts(),
+        courses: db.get(cookies.get("sessionid")),
     };
 }
 
 export const actions = {
-    default: async ({ request }) => {
+    default: async ({ cookies, request }) => {
         const data = await request.formData();
+        const sessionid = uuid();
+        cookies.set("sessionid", sessionid, { path: "/" });
 
+        const user = new User();
         await user.login(data.get("district"), "GA", data.get("username"), data.get("password"));
         const courses = await user.getCourses(0);
+
+        db.set(sessionid, courses);
 
         return courses;
     },
