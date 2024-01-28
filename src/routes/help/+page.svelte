46935a1ec9b2108bcaa193cb65592e05826466e7
Samuel Dosunmu <samuel.a.dosunmu@gmail.com>
2024-01-27 22:54:20 -0500

    Flesh out help page
    Co-authored-by: Burrows, Robert D <203363585@fcstu.org>

--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -1,9 +1,17 @@
-<p></p>
-<p> How to Add a class </p>
-<p> - click on text box that says "enter a subject..." </p>
-<p> - Type the Subject of your class </p>
-<p> How to Change grade </p>
-<p> - Look for the subject of the grade of which you plan to change </p>
-<p> - On the right of that subject should be a white text box with a grade, simply click on it and input a new grade </p>
-<p> - Or click on the up and down arrow </p>
-<p></p>
+<h2>How to Add a class</h2>
+
+<ol>
+    <li>Click on text box that says "enter a subject..."</li>
+    <li>Type the name of your subject</li>
+    <li>Press enter</li>
+</ol>
+
+<h2>How to Change a Grade</h2>
+<ul>
+    <li>Look for the subject of the grade of which you plan to change</li>
+    <li>
+        On the right of that subject should be a white text box with a grade, simply click on it and
+        input a new grade
+    </li>
+    <li>Alternatively, click on the up and down arrows</li>
+</ul>
